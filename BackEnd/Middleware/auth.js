c16edf9c2dcd7954b5939e73c226d497a3fded7e
mahmoud hassan <144733928+mahsn12@users.noreply.github.com>
2025-12-05 01:26:39 +0200

    Middle ware added

--- BackEnd/Middleware/auth.js
+++ BackEnd/Middleware/auth.js
@@ -1,4 +1,20 @@
- export const authMiddleware = ()=>{
+ import jwt from "jsonwebtoken"
+ import dot from "dotenv"
+ dot.config();
+ 
+ export const authMiddleware = (req,res,next)=>{
+    try{
+        const authorize = req.headers.authorization;
+        if(!authorize){
+           return res.status(401).json({ message: "No token provided" });
+        }
+        const token = (authorize.split(" "))[1];
+        const coded = jwt.verify(token,process.env.JWT_secret_key);
+        req.user = coded;
 
+        next();
+    } catch (error) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
 };
 
